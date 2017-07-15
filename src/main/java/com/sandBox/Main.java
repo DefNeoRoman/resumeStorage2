4947e9723d79310b3bb24b0387f54d1bf8f57a80
DefNeoRoman <d89056411060@yandex.ru>
2017-07-15 23:39:48 +0300

    now i install ruby

--- src/main/java/com/sandBox/Main.java
+++ src/main/java/com/sandBox/Main.java
@@ -8,11 +8,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Storage storage = Config.getInstance().getStorage();
-        Resume r = new Resume("Ivanov");
-        storage.clear();
-        storage.save(r);
-        System.out.println(storage.get(r.getUuid()).toString());
+
 
     }
 }
